309805400327e3f37baf508949b6c4a4d606f788
LameckMeshack <otienolameck327@gmail.com>
2024-05-09 11:24:37 +0300

    chore: Refactor wrong_live.ex template to improve guess handling

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -5,7 +5,7 @@ defmodule PentoWeb.WrongLive do
     {:ok,
      assign(socket,
        score: 0,
-       number: Enum.random(1..10),
+       secret_number: Enum.random(1..10),
        message: "Guess a number."
      )}
   end
@@ -13,6 +13,8 @@ defmodule PentoWeb.WrongLive do
   def render(assigns) do
     ~H"""
     <h1>Your score: <%= @score %></h1>
+
+    
 
     <h2>
       <%= @message %> It's <%= time() %>
@@ -25,21 +27,64 @@ defmodule PentoWeb.WrongLive do
     """
   end
 
-  def handle_event("guess", %{"number" => guess} = data, socket) do
-    IO.inspect(data)
-    number = socket.assigns.number
+  # def handle_event("guess", %{"number" => guess} = data, socket) do
+  #   IO.inspect(data)
+  #   number = socket.assigns.number
 
-    case number == String.to_integer(guess) do
-      true ->
-        message = "Your guess: #{guess}. Correct. Guess again. "
-        score = socket.assigns.score + 1
-        {:noreply, assign(socket, message: message, score: score, number: Enum.random(1..10))}
+  #   case number == String.to_integer(guess) do
+  #     true ->
+  #       message = "Your guess: #{guess}. Correct. Guess again. "
+  #       score = socket.assigns.score + 1
+  #       {:noreply, assign(socket, message: message, score: score, number: Enum.random(1..10))}
 
-      false ->
-        message = "Your guess: #{guess}. Wrong. Guess again. "
-        score = socket.assigns.score - 1
-        {:noreply, assign(socket, message: message, score: score)}
+  #     false ->
+  #       message = "Your guess: #{guess}. Wrong. Guess again. "
+  #       score = socket.assigns.score - 1
+  #       {:noreply, assign(socket, message: message, score: score)}
+  #   end
+  # end
+
+  def handle_event("guess", %{"number" => guess} = _data, socket) do
+    handle_guess(socket, guess)
+  end
+
+  def handle_guess(socket, guess) do
+    case Integer.parse(guess) do
+      {num, _} when is_integer(num) and num in 1..10 ->
+        handle_valid_guess(socket, num)
+
+      _ ->
+        handle_invalid_guess(socket)
     end
+  end
+
+  defp handle_valid_guess(socket, guess) do
+    secret_number = Enum.random(1..10)
+    correct_guess = guess == secret_number
+
+    message =
+      if correct_guess do
+        "Your guess: #{guess}. Correct. Guess again."
+      else
+        "Your guess: #{guess}. Wrong. Guess again."
+      end
+
+    score = update_score(socket.assigns.score, correct_guess)
+
+    {:noreply, assign(socket, message: message, score: score, secret_number: secret_number)}
+  end
+
+  defp handle_invalid_guess(socket) do
+    message = "Invalid guess. Please enter a number between 1 and 10."
+    {:noreply, assign(socket, message: message)}
+  end
+
+  defp update_score(score, true), do: score + 1
+  defp update_score(score, false), do: score - 1
+
+
+  def live_patch(socket, _params, _session, socket) do
+    {:ok, socket}
   end
 
   def time do
